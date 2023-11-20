8a04a98f4c4eb491330d6b3bcc79bcddf1b89e73
kozakevich13 <63108530+kozakevich13@users.noreply.github.com>
2023-11-20 19:12:01 +0200

    add new api generation

--- app.py
+++ app.py
@@ -10,7 +10,58 @@ app = Flask(__name__)
 CORS(app)
 
 # Змінні для зберігання тексту та слова
+def load_text_from_file(file_path):
+    with open(file_path, 'r', encoding='utf-8') as file:
+        text = file.read()
+    return text
 
+def load_word_from_file(word_file_path):
+    with open(file_path, 'r', encoding='utf-8') as file:
+        text = file.read()
+    return text
+file_path = 'saved_text.txt'
+
+word_file_path = 'saved_words.txt'
+
+
+
+
+def generate_message(seed_word, num_words=6):
+    # Завантаження тексту із файлу
+    text = load_text_from_file(file_path)
+
+    word = load_word_from_file(word_file_path)
+
+
+    # Токенізація тексту
+    words = nltk.word_tokenize(text)
+
+    # Визначення біграм
+    bi_grams = list(bigrams(words))
+
+    # Обчислення частот біграм
+    freq_bi_grams = FreqDist(bi_grams)
+
+    message = [seed_word]
+    current_word = seed_word
+
+    for _ in range(num_words - 1):
+        # Вибір біграм, що починається поточним словом
+        next_words = [word[1] for word in freq_bi_grams if word[0] == current_word]
+
+        if next_words:
+            next_word = next_words[0]
+            message.append(next_word)
+
+            # Перевірка, чи є крапка у згенерованому повідомленні
+            if '.' in next_word:
+                break
+
+            current_word = next_word
+        else:
+            break
+
+    return ' '.join(message)
 
 @app.route('/', methods=['POST'])
 def process_text():
@@ -44,15 +95,12 @@ def process_word():
 @app.route('/generation', methods=['GET'])
 def process_generation():
     try:
-        # Зчитати слова з файлу saved_words.txt
-        with open('saved_words.txt', 'r', encoding='utf-8') as file:
-            words = file.read().split()
-        
-        # Зчитати текст з файлу saved_text.txt
-        with open('saved_text.txt', 'r', encoding='utf-8') as text_file:
-            saved_text = text_file.read()
-            
-        return jsonify(success=True, message={'words': words, 'saved_text': saved_text})
+        seed_word = request.args.get('seed_word', '')  # Отримати seed_word з параметрів запиту
+        num_words = int(request.args.get('num_words', 6))  # Отримати num_words з параметрів запиту
+
+        message = generate_message(seed_word, num_words)
+
+        return jsonify(success=True, message=message)
     except Exception as e:
         return jsonify(success=False, message=str(e))
 
